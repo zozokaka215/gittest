a220521c86c6d267239e40ee0ef5943f37034402
root <root@ip-172-31-17-85.ec2.internal>
2024-03-05 02:13:43 +0000

    changed i value to 20

--- login.java
+++ login.java
@@ -3,7 +3,7 @@ Public class login
    public static void main()
    {
        int i =0;
-       for (i=0;i<==10;i++)
+       for (i=0;i<==20;i++)
        {
 	    System.out Println ("Number: "+i);
        }
